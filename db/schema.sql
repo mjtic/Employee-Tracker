7689bb89be3a27ef2fb48c3396bd44374add7b70
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-31 14:59:37 -0600

    adding schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+-- Write your schema here --
+
+DROP DATABASE IF EXISTS orgchart_db;
+CREATE DATABASE orgchart_db;
+
+USE orgchart_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (65, 30) NOT NULL AUTO_INCREMENT,
+  department_id INT NOT NULL AUTO_INCREMENT 
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL AUTO_INCREMENT,
+  manager_id INT NOT NULL AUTO_INCREMENT
+);
