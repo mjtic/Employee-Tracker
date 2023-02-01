8f0d56c0006e614ee9189d049e6461b8ecc5fa21
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-31 20:13:25 -0600

    added few notes in the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,22 @@
 -- Write your schema here --
 
-DROP DATABASE IF EXISTS orgchart_db;
-CREATE DATABASE orgchart_db;
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 
-USE orgchart_db;
-
+USE employee_tracker_db;
+-- Dept. Table
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL,
 );
-
+-- Role Table
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (65, 30) NOT NULL AUTO_INCREMENT,
   department_id INT NOT NULL AUTO_INCREMENT --foreign key
 );
-
+-- Employee table
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
