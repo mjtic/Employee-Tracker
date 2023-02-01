0257a6dd39e722b1015c838b62935a73536c2180
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-31 20:07:08 -0600

    pseudo coding and added switch cases, prompt quesitons, and set up few functions.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//inquirer
 const inquirer = require("inquirer");
+//console.table
 require("console.table");
 
 function startMenu() {
@@ -7,26 +9,65 @@ function startMenu() {
       {
         type: "list",
         name: "userChoice",
-        message: "Please Choose an Action:",
+        message: "Please choose your menu:",
         choices: [
-          "Get all employees.",
-          "Get all roles.",
-          "Get all departments.",
+          "View all departments.",
+          "View all roles.",
+          "View all employees.",
+          "Add a departmnet.",
+          "Add a role.",
+          "Add an employee.",
+          "Update Employee role"
         ],
       },
     ])
     .then(function (answers) {
       switch (answers.userChoice) {
-        case "Get all employees.":
-          getAllEmps();
+        case "View all departments.":
+          viewAllDepts();
           break;
-        case "Get all employees.":
-          getAllEmps();
+        case "View all roles.":
+          viewAllRoles();
           break;
+        case "View all employees.":
+          viewAllEmps();
+          break;
+        case "Add a department.":
+          AddDept();
+          break;
+        case "Add a role.":
+          AddRole();
+          break;
+        case "Add an employee.":
+          AddEmp();
+          break;          
+        case "Update employee role.":
+          UpdateEmp();
+          break;     
         default:
           break;
       }
     });
 }
+
+//View all department function
+function viewAllDepts (){
+//formatted table showing dept. names and dept. ids
+    return startMenu();
+}
+//View all role function
+function viewAllRoles(){
+//job title, role id, dept., salary
+    return startMenu();
+}
+//View all employee funciton
+function viewAllEmps (){
+//formatted table showing employee data (ids, first_name, last_name, job_titles, dept, salary, manager(supervisor))
+    return startMenu();
+}
+
+
+
+
 
 startMenu();
