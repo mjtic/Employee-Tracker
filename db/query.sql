96a93ee27c456e5ebbfda10d7731364d0e89e314
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-02-02 22:11:37 -0600

    working on 3rd query, its near impossible

--- db/query.sql
+++ db/query.sql
@@ -10,6 +10,14 @@ FROM employee;
 
 SELECT name AS Dept_NAME, id AS Dept_ID FROM department;
 
-SELECT roles.id AS id, roles.title AS title, department.name AS department, roles.salary AS salary FROM roles JOIN department ON roles.department_id = department.id
+SELECT roles.title AS Job_Title, roles.id AS Role_ID, department.name AS Department, roles.salary AS Salaries FROM roles JOIN department ON roles.department_id = department.id;
+
+job title, role id, the department that role belongs to, and the salary for that role
+
+SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department;
+ 
+
+                                            employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
 
 
