1c954afb96fcb058a0f6434ad4d5043bd4c8b645
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-02-03 22:57:39 -0600

    can't figure out addRole function

--- db/query.sql
+++ db/query.sql
@@ -13,3 +13,8 @@ SELECT name AS Dept_NAME, id AS Dept_ID FROM department;
 SELECT roles.title AS Job_Title, roles.id AS Role_ID, department.name AS Department, roles.salary AS Salaries FROM roles JOIN department ON roles.department_id = department.id;
 
 SELECT employee.id AS Employee_ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, roles.title AS Title, department.name AS Department, roles.salary AS Salaries, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department department ON department.id = roles.department_id LEFT JOIN employee manager ON manager.id = employee.manager_id;
+
+enter the name of the department and that department is added to the database
+
+INSERT INTO department (name) VALUES ("Operation")
+    
--- index.js
+++ index.js
@@ -39,6 +39,8 @@ const connection = mysql.createConnection(
 connection.connect(function (err) {
   if (err) throw err;
 });
+const utils = require("util");
+connection.query = utils.promisify(connection.query);
 
 function startMenu() {
   inquirer
@@ -51,7 +53,7 @@ function startMenu() {
           "View all departments.",
           "View all roles.",
           "View all employees.",
-          "Add a departmnet.",
+          "Add a department.",
           "Add a role.",
           "Add an employee.",
           "Update Employee role",
@@ -71,16 +73,16 @@ function startMenu() {
           viewAllEmps();
           break;
         case "Add a department.":
-          AddDept();
+          addDept();
           break;
         case "Add a role.":
-          AddRole();
+          addRole();
           break;
         case "Add an employee.":
-          AddEmp();
+          addEmp();
           break;
         case "Update employee role.":
-          UpdateEmp();
+          updateEmp();
           break;
         case "Exit.":
           connection.end();
@@ -90,46 +92,102 @@ function startMenu() {
 }
 
 //View all department function
-function viewAllDepts() {
-  connection.query(
-    "SELECT name AS Dept_NAME, id AS Dept_ID FROM department",
-    function (err, results) {
-      if (err) {
-        console.log(err);
-      }
-      console.table(results);
-      return startMenu();
-    }
+async function viewAllDepts() {
+  const allDepts = await connection.query(
+    "SELECT name AS Dept_NAME, id AS Dept_ID FROM department"
   );
-  //formatted table showing dept. names and dept. ids
+  console.table(allDepts);
+  return startMenu();
 }
+
+//formatted table showing dept. names and dept. ids
+
 //View all role function
-function viewAllRoles() {
-  connection.query(
-    "SELECT roles.id AS Role_ID, roles.title AS Job_Title, department.name AS Department, roles.salary AS Salaries FROM roles JOIN department ON roles.department_id = department.id",
-    function (err, results){
-      if (err) {
-        console.log (err);
-      }
-      console.table(results);
-      return startMenu();
-    }
-  )
-  //job title, role id, dept., salary
+async function viewAllRoles() {
+  const allRoles = await connection.query(
+    "SELECT roles.id AS Role_ID, roles.title AS Job_Title, department.name AS Department, roles.salary AS Salaries FROM roles JOIN department ON roles.department_id = department.id"
+  );
+  console.table(allRoles);
+  return startMenu();
 }
+//job title, role id, dept., salary
 //View all employee funciton
-function viewAllEmps() {
-  connection.query(
-    "SELECT employee.id AS Employee_ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, roles.title AS Title, department.name AS Department, roles.salary AS Salaries, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department department ON department.id = roles.department_id LEFT JOIN employee manager ON manager.id = employee.manager_id;",
-  function (err, results){
-    if(err) {
-      console.log(err);
-    }
-    console.table(results);
-    return startMenu();
-   }
-  )
-  //formatted table showing employee data (ids, first_name, last_name, job_titles, dept, salary, manager(supervisor))
+async function viewAllEmps() {
+  const allEmployee = await connection.query(
+    "SELECT employee.id AS Employee_ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, roles.title AS Title, department.name AS Department, roles.salary AS Salaries, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department department ON department.id = roles.department_id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
+  );
+  console.table(allEmployee);
+  return startMenu();
 }
+
+async function addDept() {
+  const answer = await inquirer.prompt([
+    {
+      type: "input",
+      name: "newDepartment",
+      message: "What is the name of the new department?",
+    },
+  ]);
+  const newDept = await connection.query(
+    `INSERT INTO department (name) VALUES ("${answer.newDepartment}")`
+  );
+  console.log(
+    `Successfully added the ${answer.newDepartment} into departments`
+  );
+  startMenu();
+}
+//prompted to enter the name, salary, and department for the role and that role is added to the database
+
+async function addRole() {
+  const addNewRoles = await connection.query("SELECT id AS VALUE, name FROM department");
+  let finalRoles = addNewRoles.map(({ id, name}) => ({
+    value: id,
+    name: `${name}`, 
+  }));
+  console.log(addRole);
+  const answer = await inquirer.prompt([
+    {
+      type: "input",
+      name: "newRole",
+      message: "What is the name of the new role?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the salary of the new role?",
+    },
+    {
+      type: "list",
+      name: "department",
+      choices: finalRoles,
+    },
+  ]);
+  const newRole = await connection.query(
+    `INSERT INTO roles (title, salary) VALUES ("${answer.newRole}","${answer.salary}), INSERT INTO department (name) VALUES ("${answer.department}")`
+  );
+  console.table(newRole);
+  console.log(`Successfully added the ${answer.newRole} into roles`);
+  startMenu();
+}
+
+//formatted table showing employee data (ids, first_name, last_name, job_titles, dept, salary, manager(supervisor))
+
+// async function addDept() {
+//   const userInput = await inquirer.prompt([
+//     {
+//      type: 'input',
+//      name: 'addDept',
+//      message: 'Please enter the department you want to add'
+//     }
+//   ])
+//   // const sql = `INSERT INTO department (name) VALUES (?)`;
+//   // const params = userInput.addDept;
+//   const newDept = await connection.query(`INSERT INTO department (name) VALUES "${userInput.addDept}"` );
+
+//   console.table(newDept);
+//         return startMenu();
+//       }
+// }
+// }
 
 startMenu();
